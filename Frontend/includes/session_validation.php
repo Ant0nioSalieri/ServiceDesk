b7df8e8565056afdc936a5f1937b97fd2ebd82d0
MartinCarreno <martincarreno71@gmail.com>
2025-06-15 22:38:33 -0400

    Actualizacion de session
    Ahora la ruta si funciona cuando uno intenta entrar a la fuerza por link

--- Frontend/includes/session_validation.php
+++ Frontend/includes/session_validation.php
@@ -3,7 +3,7 @@ session_start();
 
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['token'])) {
-    header('Location: ../templates/login.php'); // Redirigir al login si no hay token
+    header('Location: ../../templates/login.php'); // Redirigir al login si no hay token
     exit();
 }
 
@@ -20,14 +20,14 @@ $context = stream_context_create($options);
 $response = @file_get_contents($url, false, $context); // Usa @ para suprimir errores
 if ($response === false) {
     session_destroy(); // Eliminar la sesión si no se puede validar el token
-    header('Location: ../templates/login.php');
+    header('Location: ../../templates/login.php');
     exit();
 }
 
 $result = json_decode($response, true);
 if (!$result || !$result['valid']) {
     session_destroy(); // Eliminar la sesión si el token no es válido
-    header('Location: ../templates/login.php');
+    header('Location: ../../templates/login.php');
     exit();
 }
 ?>
